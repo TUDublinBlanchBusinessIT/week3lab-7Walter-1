0b6e5d75cf932bc646755e690313b6108d32ac11
B00145274 Walter O Hara <B00145274@mytudublin.ie>
2023-10-04 10:05:18 +0100

    rows of 5

---
+++ LoopInFives.php
@@ -0,0 +1,14 @@
+<?php
+#phpForLoop -week3lab-7Walter-1
+#Walter O'Hara 04/10/2023 
+#this program uses a loop to count up to 10 and puts the value of the loop counter out to the screen on each iteration
+for ($i=1;$i<=20;$i++) {
+    echo "$i, ";
+    if($i%5==0)
+    {
+        echo"<br>";
+    }
+}
+
+?>
+
