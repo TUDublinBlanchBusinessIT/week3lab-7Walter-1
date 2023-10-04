f67a6a84f1a7815bb890e9e3b3df43d17229f86c
B00145274 Walter O Hara <B00145274@mytudublin.ie>
2023-10-04 09:36:11 +0100

    count up 20

--- phpForLoop.php
+++ phpForLoop.php
@@ -1,9 +1,8 @@
 <?php
-#phpForLoop - put the name of the file/program here - notes the hashtag indicates a comment in PHP
-#put your name and the date here
-#put a descripition of what the code does here - 
+#phpForLoop -week3lab-7Walter-1
+#Walter O'Hara 04/10/2023 
 #this program uses a loop to count up to 10 and puts the value of the loop counter out to the screen on each iteration
-for ($i=1;$i<=10;$i++) {
+for ($i=1;$i<=20;$i++) {
     echo "$i, ";
 }
 
